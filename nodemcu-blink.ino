3e7c405d093b6f9392defc1dea099bca627c8b75
khurram <khuziz@hotmail.com>
2017-02-04 13:12:41 +0500

    reconnect wifi

--- nodemcu-blink.ino
+++ nodemcu-blink.ino
@@ -1,5 +1,4 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
@@ -7,21 +6,14 @@
 const char* ssid = "";
 const char* password = "";
 const char* mdnsName = "nodemcu";
-
 int led = 0;
-String webPage = "";
 
 MDNSResponder mdns;
-ESP8266WebServer server(80);
+ESP8266WebServer httpServer(80);
 
-void setup() {
-  webPage += "<h1>Arduino core for ESP8266 WiFi chip</h1><p><a href=\"on\"><button>On</button></a><a href=\"off\"><button>Off</button></a></p>";  
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
-  WiFi.begin(ssid, password);
-  Serial.begin(115200); 
-  Serial.println("Setting up");
-  delay(5000);
+String webPage = "";
+
+void setupWifi() {
   // Wait for connection and flash led till it connects
   // when connected; turn the led off
   while (WiFi.status() != WL_CONNECTED) {
@@ -35,29 +27,43 @@ void setup() {
   Serial.print("Connected to ");  Serial.println(ssid);
   Serial.print("IP address: ");   Serial.println(WiFi.localIP());
 
-  // lets mDNS nodemcu on the received ip
+  // lets mDNS on the received ip
   if (mdns.begin(mdnsName, WiFi.localIP())) {
-    Serial.println("mDNS started for " + mdnsName + ".local");
+    Serial.print("mDNS started for "); Serial.print(mdnsName); Serial.println(".local");
     MDNS.addService("http", "tcp", 80);
     Serial.println("DNS-SD started for http / tcp / 80");
   }
+}
+
+void setup() {
+  webPage = "<h1>Arduino core for ESP8266 WiFi chip</h1><p><a href=\"on\"><button>On</button></a><a href=\"off\"><button>Off</button></a></p>";  
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  WiFi.begin(ssid, password);
+  Serial.begin(115200); 
+  Serial.println("Setting up");
   
-  server.on("/", []() {
-    server.send(200, "text/html", webPage);
+  setupWifi();
+  
+  httpServer.on("/", []() {
+    httpServer.send(200, "text/html", webPage);
   });
-  server.on("/on", []() {
-    server.send(200, "text/html", webPage);
+  httpServer.on("/on", []() {
+    httpServer.send(200, "text/html", webPage);
     digitalWrite(led, HIGH);
   });
-  server.on("/off", [](){
-    server.send(200, "text/html", webPage);
+  httpServer.on("/off", [](){
+    httpServer.send(200, "text/html", webPage);
     digitalWrite(led, LOW);
   });
-  
-  server.begin();
+
+  //httpServer will continue to work across disconnection
+  httpServer.begin();
   Serial.println("HTTP server started");
 }
  
 void loop() {
-  server.handleClient();
+  if (WiFi.status() != WL_CONNECTED)
+    setupWifi();
+  httpServer.handleClient();
 } 
